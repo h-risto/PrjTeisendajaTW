4c4b104eb0b6cb7a999f23c93473799d88a01d08
risto <risto@PC>
2022-02-27 23:00:36 +0200

    Tükeldatud kiiruse teisendaja eraldi liidesesse ja klassi

--- PrjTeisendajad/CTeisendaja.vb
+++ PrjTeisendajad/CTeisendaja.vb
@@ -10,13 +10,4 @@
         Return mi * KM2MI
     End Function
 
-
-
-    Public Function KmhToMs(kmh As Double) As Double Implements ITeisendaja.KmhToMs
-        Throw New NotImplementedException()
-    End Function
-
-    Public Function MsToKmh(ms As Double) As Double Implements ITeisendaja.MsToKmh
-        Throw New NotImplementedException()
-    End Function
 End Class
--- PrjTeisendajad/ITeisendaja.vb
+++ PrjTeisendajad/ITeisendaja.vb
@@ -2,7 +2,5 @@
     Function KilometersToMiles(ByVal km As Double) As Double
     Function MilesToKilometers(ByVal mi As Double) As Double
 
-    Function KmhToMs(ByVal kmh As Double) As Double
-    Function MsToKmh(ByVal ms As Double) As Double
 
 End Interface
