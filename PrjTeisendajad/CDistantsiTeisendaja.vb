7b87dee3ca1eba76bc9ea4f5c304882fa14cb69a
risto <risto@PC>
2022-02-28 09:24:37 +0200

    muudetud komponendi meetodite nähtavusskoobid privaatseks

--- PrjTeisendajad/CDistantsiTeisendaja.vb
+++ PrjTeisendajad/CDistantsiTeisendaja.vb
@@ -2,11 +2,11 @@
     Implements IDistantsiTeisendaja
 
     Const KM2MI = 1.6093
-    Public Function KilometersToMiles(km As Double) As Double Implements IDistantsiTeisendaja.KilometersToMiles
+    Private Function KilometersToMiles(km As Double) As Double Implements IDistantsiTeisendaja.KilometersToMiles
         Return km / KM2MI
     End Function
 
-    Public Function MilesToKilometers(mi As Double) As Double Implements IDistantsiTeisendaja.MilesToKilometers
+    Private Function MilesToKilometers(mi As Double) As Double Implements IDistantsiTeisendaja.MilesToKilometers
         Return mi * KM2MI
     End Function
 
--- PrjTeisendajad/CKiiruseTeisendaja.vb
+++ PrjTeisendajad/CKiiruseTeisendaja.vb
@@ -1,11 +1,11 @@
 ﻿Public Class CKiiruseTeisendaja
     Implements IKiiruseTeisendaja
 
-    Public Function KmhToMs(kmh As Double) As Double Implements IKiiruseTeisendaja.KmhToMs
+    Private Function KmhToMs(kmh As Double) As Double Implements IKiiruseTeisendaja.KmhToMs
         Throw New NotImplementedException()
     End Function
 
-    Public Function MsToKmh(ms As Double) As Double Implements IKiiruseTeisendaja.MsToKmh
+    Private Function MsToKmh(ms As Double) As Double Implements IKiiruseTeisendaja.MsToKmh
         Throw New NotImplementedException()
     End Function
 End Class
